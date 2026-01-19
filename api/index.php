fc6c98c176117e669ea3b65aa19dda6d511e3b34
mazroni9 <zahrma0p@yahoo.com>
2026-01-19 04:18:01 +0300

    Fix api/index.php to use Laravel bootstrap correctly

--- api/index.php
+++ api/index.php
@@ -1,13 +1,20 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+define('LARAVEL_START', microtime(true));
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
